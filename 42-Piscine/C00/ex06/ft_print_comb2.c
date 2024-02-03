1d7025b07f5ef268b4b008157dafd85a96bbd577
Ertugrul Likos <elikos@k1m14s04.42istanbul.com.tr>
2024-02-03 18:04:11 +0300

    last

--- 42-Piscine/C00/ex06/ft_print_comb2.c
+++ 42-Piscine/C00/ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: elikos <elikos@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 04:56:11 by elikos            #+#    #+#             */
-/*   Updated: 2024/01/29 11:15:26 by elikos           ###   ########.fr       */
+/*   Updated: 2024/01/30 18:50:07 by elikos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void	ft_print_comb2(void)
 			ft_putchar((first / 10) + 48);
 			ft_putchar((first % 10) + 48);
 			ft_putchar(' ');
-			ft_putchar((first / 10) + 48);
+			ft_putchar((second / 10) + 48);
 			ft_putchar((second % 10) + 48);
 			if (first != 98)
 			{
